b36e626dd93c06392f428db2d00c9e4b35208ae3
Akshat <respectthegame23@gmail.com>
2018-11-19 19:36:32 -0600

    index styling

--- header.php
+++ header.php
@@ -12,74 +12,44 @@ echo "<pre>".$_SESSION['username']."</pre>";
  ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/timer.js" charset="utf-8"></script>
+
 <style media="screen">
   nav a{
       margin-right: 1em;
     }
-
 </style>
 
 <header>
 <title>Tompkins CS</title>
-  <!-- Display the countdown timer in an element -->
-<p id="timer" style="display: inline">Null</p>
+    <!-- Display the countdown timer in an element -->
+    <p id="timer" style="display: inline">Null</p>
 
 
- <!-- links for other pages -->
- <nav id = 'tabs' style="float: right">
-  <a href=<?php echo $dir."index.php" ?>>Home</a>
+   <!-- links for other pages -->
+   <nav id = 'tabs' style="float: right">
+    <a href=<?php echo $dir."index.php" ?>>Home</a>
 
-   <a href=<?php echo $dir."menu.php" ?>>Menu</a>
+     <a href=<?php echo $dir."menu.php" ?>>Menu</a>
 
-   <a href = <?php echo $dir."vieworders.php" ?>>View Orders</a>
+     <a href = <?php echo $dir."vieworders.php" ?>>View Orders</a>
+     <?php if($_SESSION['perm']==100){ ?>
+       <a href=<?php echo $dir."writtenScore.php" ?>>Writtens</a>
+   <?php } ?>
+
    <?php if($_SESSION['perm']==100){ ?>
-     <a href=<?php echo $dir."writtenScore.php" ?>>Writtens</a>
- <?php } ?>
+     <a href=<?php echo $dir."addTeam.php" ?>>Add Team</a>
+  <?php } ?>
+  <?php if($_SESSION['perm']==100){ ?>
+    <a href=<?php echo $dir."uploads/gradeProblems.php" ?>>Grade Problems</a>
+  <?php } ?>
+  <?php if($_SESSION['perm']==1){ ?>
+    <a href=<?php echo $dir."submitProblem.php" ?>>Submit Problems</a>
+  <?php } ?>
 
- <?php if($_SESSION['perm']==100){ ?>
-   <a href=<?php echo $dir."addTeam.php" ?>>Add Team</a>
-<?php } ?>
-<?php if($_SESSION['perm']==100){ ?>
-  <a href=<?php echo $dir."uploads/gradeProblems.php" ?>>Grade Problems</a>
-<?php } ?>
-<?php if($_SESSION['perm']==1){ ?>
-  <a href=<?php echo $dir."submitProblem.php" ?>>Submit Problems</a>
-<?php } ?>
+     <a href=<?php echo $dir."downloads.php" ?>>Downloads</a>
 
-   <a href=<?php echo $dir."downloads.php" ?>>Downloads</a>
+     <a href=<?php echo $dir."logout.php" ?>>Logout</a>
+   </nav>
 
-   <a href=<?php echo $dir."logout.php" ?>>Logout</a>
- </nav>
-
- <script>
- // Set the date we're counting down to
- var countDownDate = new Date("September 19, 2018 16:40:00").getTime();
-
- // Update the count down every 1 second
- var x = setInterval(function() {
-
-   // Get todays date and time
-   var now = new Date().getTime();
-
-   // Find the distance between now an the count down date
-   var distance = countDownDate - now;
-
-   // Time calculations for days, hours, minutes and seconds
-   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-   // Display the result in the element with id="demo"
-   document.getElementById("timer").innerHTML = hours + "h "
-   + minutes + "m " + seconds + "s ";
-
-   // If the count down is finished, write some text
-   if (distance < 0) {
-     clearInterval(x);
-     document.getElementById("timer").innerHTML = "EXPIRED";
-     $("#problemSubmit").prop('disabled', true)
-   }
- }, 1000);
- </script>
 </header>
--- index.php
+++ index.php
@@ -1,54 +1,103 @@
-<?php $dir = ""; include($dir . "header.php"); ?>
+<html>
+<head>
+  <title>Tompkins CS</title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
-<style media="screen">
 
-  table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    margin-bottom: 3em;
-    margin-top: 3em;
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <!--===============================================================================================-->
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!--===============================================================================================-->
+  <script src="vendor/select2/select2.min.js"></script>
+  <link rel="stylesheet" href="/css/index.css">
+</head>
+
+
+<body>
+<?php
+
+  // redirecting to login page if not logged in
+  session_start();
+  if(!isset($_SESSION['username'])){
+     ob_start();
+     header('Location: '.'login.php');
+     ob_end_flush();
+     die();
   }
-
-  td,th {
-    /* border: 1px dashed #aaa; */
-    text-align: center;
-    padding: 1em;
-    overflow: auto;
-  }
-  tr:nth-child(even) {
-    background-color: #ccc;
-  }
-
-  #members{
-      width: 20%;
-  }
-  #members td,th{
-    border: 1px dashed #aaa;
-
-  }
-  #rankings{
-      width: 10%;
-
-  }
-  #rankings td,th{
-    border: 3px dotted #ddd ;
-  }
-</style>
-<!-- content -->
-
-
-<h1>TOMPKINS COMPUTER SCIENCE COMPETITION </h1>
+  $dir="";
+?>
 
 
 
+  <div class="overlay"></div>
+
+  <img src="img/back.jpg" alt="Background for Homepage" id = "video">
+
+  <div class="masthead">
+    <div class="masthead-bg"></div>
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto">
+          <div class="masthead-content text-white py-5 py-md-0">
+            <h1 class="mb-3">Welcome to the Tompkins Computer Science Competition, <?php echo $_SESSION['username'] ?></h1>
+
+            <!-- links for other pages -->
+            <nav id = 'menu' >
+             <a href=<?php echo $dir."index.php" ?>>Home</a><br>
+
+              <a href=<?php echo $dir."menu.php" ?>>Menu</a><br>
+
+              <a href = <?php echo $dir."vieworders.php" ?>>View Orders</a><br>
+              <?php if($_SESSION['perm']==100){ ?>
+                <a href=<?php echo $dir."writtenScore.php" ?>>Writtens</a><br>
+            <?php } ?>
+
+            <?php if($_SESSION['perm']==100){ ?>
+              <a href=<?php echo $dir."addTeam.php" ?>>Add Team</a><br>
+           <?php } ?>
+           <?php if($_SESSION['perm']==100){ ?>
+             <a href=<?php echo $dir."uploads/gradeProblems.php" ?>>Grade Problems</a><br>
+           <?php } ?>
+           <?php if($_SESSION['perm']==1){ ?>
+             <a href=<?php echo $dir."submitProblem.php" ?>>Submit Problems</a><br>
+           <?php } ?>
+
+              <a href=<?php echo $dir."downloads.php" ?>>Downloads</a><br>
+
+              <a href=<?php echo $dir."logout.php" ?>>Logout</a>
+            </nav>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<div class = "tables">
+  <ul class="list-unstyled text-center mb-0">
 <!-- info such as rank, problems submitted, member scores, admin released information, etc-->
 
 <!-- if a team show members, add members,show member scores -->
 <?php
 if ($_SESSION['perm']==1) {
-
-
-
 
   $servername = "localhost";
   $username = "root";
@@ -66,10 +115,9 @@ if ($_SESSION['perm']==1) {
   //add member in database
 
   if(isset($_POST['sub'])){
-    $sql = "INSERT INTO members(name,team) VALUES ('".$_POST['memberName']."','".$_SESSION['username']."')";
-    $result = $conn->query($sql);
-    header('Location:index.php');
-
+      $sql = "INSERT INTO members(name,team) VALUES ('".$_POST['memberName']."','".$_SESSION['username']."')";
+      $result = $conn->query($sql);
+      header('Location:index.php');
     }
 
 
@@ -77,9 +125,22 @@ if ($_SESSION['perm']==1) {
   $sql = "SELECT * FROM members WHERE team='".$_SESSION['username']."';";
 
   $result = $conn->query($sql);
+  if($result->num_rows<3){
 
+  ?>
+  <li class='list-unstyled-item'>
+    <!--register members-->
+    <form action="index" method="post">
+          <label for="memberName" style="font-weight:800"> Member Name: </label>
+          <input type="text" name="memberName" placeholder="John Doe">
+        <input type="submit" name="sub" value="Register">
+    </form>
+  </li>
+
+  <?php
+  }
   if($result->num_rows>0){
-    echo "<table id = 'members'><caption>Competitors</caption>";
+    echo "<li class='list-unstyled-item'><table id = 'members'><caption>Competitors</caption><th>Name</th><th>Score</th>";
     while($row = $result->fetch_assoc()) {
       echo "<tr>
       <td>".$row['name']."</td>";
@@ -91,27 +152,18 @@ if ($_SESSION['perm']==1) {
       }
       echo "</tr>";
     }
-    echo "</table>";
+    echo "</table></li>";
   }
 
 
 
 $conn->close();
 
-if($result->num_rows<3){
+}
+
 ?>
 
 
-
-<!--register members-->
-<form action="index" method="post">
-      <label for="memberName">Member Name: </label>
-      <input type="text" name="memberName" placeholder="John Doe">
-    <input type="submit" name="sub" value="Register Member">
-</form>
-<?php }} ?>
-
-<!-- if an admin, set written scores-->
 
 
 
@@ -141,10 +193,6 @@ if($result->num_rows<3){
       $level = $row['level'];
     }
 
-
-
-
-
   //display members already in databse
   $sql = "SELECT * FROM teams WHERE level='".$level."' ORDER BY points DESC, problemsWrong, username;";
   if($level == null){
@@ -155,12 +203,12 @@ if($result->num_rows<3){
   $nov=true;
   if($result->num_rows>0){
     if($level=='adv'||$_SESSION['perm']==100){
-      echo "<table style = 'display: inline;' id = 'rankings'><th>Team</th><th>Points</th><th>Right</th>";
-      echo "<caption>Advanced Rankings</caption>";
+      echo "<li class='list-unstyled-item'><p class = 'caption'>Advanced Rankings</p><div class = 'scrollit'><table id = 'rankings'><thead><th>Team</th><th>Points</th><th>Right</th></thead><tbody>";
+
     }
     while($row = $result->fetch_assoc()) {
       if($row['level']=='nov'&&$nov == true){
-        echo "</table><table style = 'float:right;' id = 'rankings'><caption>Novice Rankings</caption><th>Team</th><th>Points</th><th>Right</th>";
+        echo "</tbody></table></div></li><li class='list-unstyled-item'><p class = 'caption'>Novice Rankings</p><div class = 'scrollit'><table style='margin-left:20px;' id = 'rankings'><thead><th>Team</th><th>Points</th><th>Right</th></tead><tbody>";
         $nov = false;
       }
       echo "<tr>
@@ -169,7 +217,15 @@ if($result->num_rows<3){
       echo "<td>".$row['problemsRight']."</td>";
       echo "</tr>";
     }
-    echo "</table>";
+    echo "</tbody></table></div></li>";
   }
 
  ?>
+    </ul>
+  </div>
+</body>
+</html>
+
+<script type="text/javascript">
+  $('table').scrollTableBody();
+</script>
